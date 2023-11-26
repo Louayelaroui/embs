d46b6fa139c9f6efeab8206b3c4993d043e165b1
azzachtioui17 <azzachtioui17@gmail.com>
2023-11-26 13:41:06 +0100

    added api point

--- lib/pages/auth/SignIn.dart
+++ lib/pages/auth/SignIn.dart
@@ -1,86 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import '../../reposetories/constants.dart';
 import '../components/custom_btn.dart';
 import '../components/custom_input_field.dart';
 import '../components/header.dart';
 
-
-class SignIn extends StatelessWidget {
+class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
+
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-
           Positioned.fill(
             child: Header(),
           ),
-
           Positioned.fill(
             top: MediaQuery.of(context).padding.top + 10,
             child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'SignIn',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 40),
-
-                  Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 10.0),
-                            child: Image.asset(
-                              'assets/images/SignIn.png',
-                              height: 75,
-                              width: 80,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          CustomInputField(
-                            title: 'Email',
-                            hintTextKey: 'enter your email',
-                            inputColor: Colors.blue,
-                            hintColor: Colors.grey,
-
-                          ),
-
-                          CustomInputField(
-                            title: 'Password',
-                            hintTextKey: 'enter your password',
-                            inputColor: Colors.blue,
-                            hintColor: Colors.grey,
-                            obscureText: true,
-
-                          ),
-
-                          CustomBtn(
-                            text: 'SignIn',
-                            onPress: () { },
-                            primary: true,
-                            textColors: Colors.white,
-                            buttonColor: SignInColor,
-                          ),
-                        ],
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'SignIn',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
                       ),
-
-                  ),
-                ],
+                    ),
+                    SizedBox(height: 40),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          child: Image.asset(
+                            'assets/images/SignIn.png',
+                            height: 75,
+                            width: 80,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        CustomInputField(
+                          controller: emailController,
+                          title: 'Email',
+                          hintTextKey: 'enter your email',
+                          inputColor: Colors.blue,
+                          hintColor: Colors.grey,
+                        ),
+                        CustomInputField(
+                          controller: passwordController,
+                          title: 'Password',
+                          hintTextKey: 'enter your password',
+                          inputColor: Colors.blue,
+                          hintColor: Colors.grey,
+                          obscureText: true,
+                        ),
+                        CustomBtn(
+                          text: 'SignIn',
+                          onPress: () {
+                            signInUser();
+                          },
+                          primary: true,
+                          textColors: Colors.white,
+                          buttonColor: SignInColor,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  Future<void> signInUser() async {
+    final url = 'the api endpoint';
+
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: {
+          'email': emailController.text,
+          'password': passwordController.text,
+        },
+      );
+
+      if (response.statusCode == 200) {
+
+        print('User signed in successfully!');
+
+      } else {
+
+        print('Error: ${response.statusCode}');
+        print('Body: ${response.body}');
+
+      }
+    } catch (error) {
+
+      print('Error: $error');
+
+    }
   }
 }
--- lib/pages/auth/SignUp.dart
+++ lib/pages/auth/SignUp.dart
@@ -1,41 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import '../../reposetories/constants.dart';
 import '../components/custom_btn.dart';
 import '../components/custom_input_field.dart';
 import '../components/header.dart';
 
-
-class SignUp extends StatelessWidget {
+class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
+
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController repeatPasswordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-
           Positioned.fill(
             child: Header(),
           ),
-
           Positioned.fill(
             top: MediaQuery.of(context).padding.top + 10,
             child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'SignUp',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'SignUp',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 40),
-
-                  Expanded(
-                    child: Column(
+                    SizedBox(height: 40),
+                    Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Padding(
@@ -48,55 +55,83 @@ class SignUp extends StatelessWidget {
                           ),
                         ),
                         CustomInputField(
+                          controller: nameController,
                           title: 'Name',
                           hintTextKey: 'enter your name',
                           inputColor: Colors.blue,
                           hintColor: Colors.grey,
-
                         ),
                         CustomInputField(
+                          controller: emailController,
                           title: 'Email',
                           hintTextKey: 'enter your email',
                           inputColor: Colors.blue,
                           hintColor: Colors.grey,
-
                         ),
-
                         CustomInputField(
+                          controller: passwordController,
                           title: 'Password',
                           hintTextKey: 'enter your password',
                           inputColor: Colors.blue,
                           hintColor: Colors.grey,
                           obscureText: true,
-
                         ),
-
                         CustomInputField(
+                          controller: repeatPasswordController,
                           title: 'Repeat Password',
                           hintTextKey: 'Repeat Password',
                           inputColor: Colors.blue,
                           hintColor: Colors.grey,
                           obscureText: true,
-
                         ),
-
                         CustomBtn(
                           text: 'SignUp',
-                          onPress: () { },
+                          onPress: () {
+                            signUpUser();
+                          },
                           primary: true,
                           textColors: Colors.white,
                           buttonColor: SignInColor,
                         ),
                       ],
                     ),
-
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  Future<void> signUpUser() async {
+    final url = 'the api endpoint';
+
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: {
+          'name': nameController.text,
+          'email': emailController.text,
+          'password': passwordController.text,
+        },
+      );
+
+      if (response.statusCode == 200) {
+
+        print('User signed up successfully!');
+
+      } else {
+
+        print('Error: ${response.statusCode}');
+        print('Body: ${response.body}');
+
+      }
+    } catch (error) {
+
+      print('Error: $error');
+
+    }
   }
 }
